9fd2bac1928f3125004fb5c9aa6d48f21ca9f75c
2003HEMANTHL <hemanth.l@hostupcloud.com>
2025-07-13 13:19:39 +0530

    new logger file

---
+++ src/exception.py
@@ -0,0 +1,22 @@
+import sys
+
+
+def error_message_detail(error, error_detail):
+    _, _, exc_tb = error_detail.exc_info()
+    if exc_tb is not None:
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        error_message = (
+            f"Error occurred in python script name [{file_name}] "
+            f"line number [{exc_tb.tb_lineno}] error message [{error}]"
+        )
+    else:
+        error_message = f"Error message [{error}] (no traceback available)"
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
+
+    def __str__(self):
+        return self.error_message
